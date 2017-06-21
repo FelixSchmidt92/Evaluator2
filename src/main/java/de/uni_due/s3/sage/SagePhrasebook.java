6c1d0b36ac3c84e2356d9d705e807e8b81deaf51
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-06-21 15:12:43 +0200

    SagePhrasebook

---
+++ src/main/java/de/uni_due/s3/sage/SagePhrasebook.java
@@ -0,0 +1,13 @@
+package de.uni_due.s3.sage;
+
+public class SagePhrasebook {
+
+	public static String OMOBJToSage(Object omobj) {
+		return null;
+
+	}
+
+	public static Object SageToOMOBJ(String sageExpression) {
+		return null;
+	}
+}
