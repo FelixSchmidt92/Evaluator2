78f7a437bc3191588ec8a85849233404bd1b4907
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-07-12 11:52:31 +0200

    added linalg2

--- src/main/java/de/uni_due/s3/evaluator/core/function/functions/linalg2/Matrix.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/functions/linalg2/Matrix.java
@@ -3,25 +3,61 @@ package de.uni_due.s3.evaluator.core.function.functions.linalg2;
 import java.util.List;
 
 import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.core.function.LinalgUtils;
+import de.uni_due.s3.evaluator.core.functionData.OMSymbol;
+import de.uni_due.s3.evaluator.exceptions.cas.CasEvaluationException;
+import de.uni_due.s3.evaluator.exceptions.cas.CasNotAvailableException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidNumberOfArgumentsException;
+import de.uni_due.s3.evaluator.exceptions.function.InvalidResultTypeException;
+import de.uni_due.s3.evaluator.exceptions.representation.NoRepresentationAvailableException;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+import de.uni_due.s3.sage.Sage;
 
 public class Matrix extends Function {
 
 	@Override
-	protected Object execute(List<Object> arguments) {
-		// TODO Auto-generated method stub
-		return null;
+	protected Object execute(List<Object> arguments) throws FunctionInvalidArgumentTypeException,
+			CasEvaluationException, FunctionInvalidNumberOfArgumentsException, CasNotAvailableException,
+			NoRepresentationAvailableException, OpenMathException, InvalidResultTypeException {
+		for (Object arg : arguments) {
+			if (!(LinalgUtils.isOMAwithOMS(arg, OMSymbol.LINALG2_MATRIXROW))) {
+				throw new FunctionInvalidArgumentTypeException(this, "matrixrow");
+			}
+		}
+
+		Object result = Sage.evaluateInCAS(getPartialSageSyntax(arguments));
+
+		if (!(LinalgUtils.isOMAwithOMS(result, OMSymbol.LINALG2_MATRIX))) {
+			throw new InvalidResultTypeException(this, "matrix");
+		}
+		return result;
 	}
 
 	@Override
 	protected int minArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
 	}
 
 	@Override
 	protected int maxArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
+	}
+
+	@Override
+	public String getPartialSageSyntax(List<Object> arguments)
+			throws FunctionInvalidNumberOfArgumentsException, NoRepresentationAvailableException {
+		String sageExpression = "matrix([";
+
+		for (int i = 0; i < arguments.size(); i++) {
+			if (i == 0) {
+				sageExpression = sageExpression + getSageSyntax(arguments.get(i));
+			} else {
+				sageExpression = sageExpression + "," + getSageSyntax(arguments.get(i));
+			}
+		}
+
+		return sageExpression + "])";
 	}
 
 }
--- src/main/java/de/uni_due/s3/evaluator/core/function/functions/linalg2/MatrixRow.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/functions/linalg2/MatrixRow.java
@@ -3,25 +3,53 @@ package de.uni_due.s3.evaluator.core.function.functions.linalg2;
 import java.util.List;
 
 import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.core.function.LinalgUtils;
+import de.uni_due.s3.evaluator.core.functionData.OMSymbol;
+import de.uni_due.s3.evaluator.exceptions.cas.CasEvaluationException;
+import de.uni_due.s3.evaluator.exceptions.cas.CasNotAvailableException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidNumberOfArgumentsException;
+import de.uni_due.s3.evaluator.exceptions.function.InvalidResultTypeException;
+import de.uni_due.s3.evaluator.exceptions.representation.NoRepresentationAvailableException;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+import de.uni_due.s3.sage.Sage;
 
 public class MatrixRow extends Function {
 
 	@Override
-	protected Object execute(List<Object> arguments) {
-		// TODO Auto-generated method stub
-		return null;
+	protected Object execute(List<Object> arguments)
+			throws CasEvaluationException, FunctionInvalidNumberOfArgumentsException, CasNotAvailableException,
+			NoRepresentationAvailableException, OpenMathException, InvalidResultTypeException {
+		Object result = Sage.evaluateInCAS(getPartialSageSyntax(arguments));
+		if (!(LinalgUtils.isOMAwithOMS(result, OMSymbol.LINALG2_MATRIXROW))) {
+			throw new InvalidResultTypeException(this, "matrixrow");
+		}
+		return result;
 	}
 
 	@Override
 	protected int minArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
 	}
 
 	@Override
 	protected int maxArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
+	}
+
+	@Override
+	public String getPartialSageSyntax(List<Object> arguments)
+			throws FunctionInvalidNumberOfArgumentsException, NoRepresentationAvailableException {
+		String sageExpression = "[";
+		
+		for (int i = 0; i < arguments.size(); i++) {
+			if (i == 0) {
+				sageExpression = sageExpression + getSageSyntax(arguments.get(i));
+			} else {
+				sageExpression = sageExpression + "," + getSageSyntax(arguments.get(i));
+			}
+		}
+		
+		return sageExpression + "]";
 	}
 
 }
