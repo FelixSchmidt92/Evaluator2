ef1e4f9df38395bff33053fba7b51c2f717d0577
Florian Richtscheid <Florian.Richtscheid@paluno.uni-due.de>
2017-07-14 19:16:01 +0200

    Implemented set and size from set1 package

--- src/main/java/de/uni_due/s3/evaluator/core/function/functions/set1/Set.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/functions/set1/Set.java
@@ -4,21 +4,25 @@ import java.util.List;
 
 
 import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.core.functionData.OMSymbol;
 import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidNumberOfArgumentsException;
 import de.uni_due.s3.evaluator.exceptions.representation.NoRepresentationAvailableException;
 import de.uni_due.s3.openmath.jaxb.OMA;
 import de.uni_due.s3.openmath.jaxb.OMS;
 
+/**
+ * Implements a list. Values can occur more than ones in this list. In a normal set values could only occur ones!.
+ * We have to implement it this way, otherwise we wouldn't be compatible with evaluator1
+ *  
+ * @author frichtscheid
+ *
+ */
 public class Set extends Function {
 
 	@Override
 	protected Object execute(List<Object> arguments) {
-		System.out.println("Set");
 		OMA oma = new OMA();
-		OMS oms = new OMS();
-		
-		oms.setCd("set1");
-		oms.setName("set");
+		OMS oms = OMSymbol.SET1_SET;
 		
 		List<Object> omel = oma.getOmel();
 		omel.add(oms);
