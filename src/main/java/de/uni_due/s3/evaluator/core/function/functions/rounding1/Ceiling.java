71949d5e7c7d6210aec34fa8695c71073e9d5937
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-07-18 17:21:15 +0200

    Added Ceiling, Floor

--- src/main/java/de/uni_due/s3/evaluator/core/function/functions/rounding1/Ceiling.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/functions/rounding1/Ceiling.java
@@ -3,26 +3,32 @@ package de.uni_due.s3.evaluator.core.function.functions.rounding1;
 import java.util.List;
 
 import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.core.function.NumberUtils;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.evaluator.exceptions.openmath.InputMismatchException;
 
 public class Ceiling extends Function{
 
 	
 	@Override
-	protected Object execute(List<Object> arguments) {
-		// TODO Auto-generated method stub
-		return null;
+	protected Object execute(List<Object> arguments) throws FunctionInvalidArgumentTypeException {
+		try {
+			double value = NumberUtils.convertOMIOMFToDouble(arguments.get(0));
+			value = Math.ceil(value);
+			return NumberUtils.convertDoubleToOMIOMF(value);
+		} catch (InputMismatchException e) {
+			throw new FunctionInvalidArgumentTypeException(this, "(0)Integer/Double/Float");
+		}
 	}
 	
 	@Override
 	protected int minArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 	@Override
 	protected int maxArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 
---
+++ src/test/java/de/uni_due/s3/evaluator/core/function/functions/rounding1/TestFloor.java
@@ -0,0 +1,103 @@
+package de.uni_due.s3.evaluator.core.function.functions.rounding1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.core.function.OMExecutor;
+import de.uni_due.s3.evaluator.core.function.functions.TestFunction;
+import de.uni_due.s3.evaluator.exceptions.cas.CasEvaluationException;
+import de.uni_due.s3.evaluator.exceptions.cas.CasNotAvailableException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidNumberOfArgumentsException;
+import de.uni_due.s3.evaluator.exceptions.parser.ParserException;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedExerciseVariableException;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedFillInVariableException;
+import de.uni_due.s3.evaluator.exceptions.representation.NoRepresentationAvailableException;
+import de.uni_due.s3.evaluator.parser.ExpressionParser;
+import de.uni_due.s3.openmath.jaxb.OMOBJ;
+import de.uni_due.s3.openmath.omutils.OMCreator;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+
+public class TestFloor extends TestFunction {
+
+	private Function func = new Floor();
+
+	@Test
+	public void testFloorInteger() throws FunctionException, CasEvaluationException, CasNotAvailableException,
+			NoRepresentationAvailableException, OpenMathException {
+		List<Object> args = new ArrayList<Object>();
+		args.add(OMCreator.createOMI(3));
+		Object result = func.evaluate(args);
+		assertEquals(OMCreator.createOMI(3), result);
+	}
+
+	@Test
+	public void testFloorFloat1() throws FunctionException, CasEvaluationException, CasNotAvailableException,
+			NoRepresentationAvailableException, OpenMathException {
+		List<Object> args = new ArrayList<Object>();
+		args.add(OMCreator.createOMF(2.5));
+		Object result = func.evaluate(args);
+		assertEquals(OMCreator.createOMI(2), result);
+	}
+	
+	@Test
+	public void testFloorFloat2() throws FunctionException, CasEvaluationException, CasNotAvailableException,
+			NoRepresentationAvailableException, OpenMathException {
+		List<Object> args = new ArrayList<Object>();
+		args.add(OMCreator.createOMF(2.8));
+		Object result = func.evaluate(args);
+		assertEquals(OMCreator.createOMI(2), result);
+	}
+
+	@Test
+	public void testFloorIntegration1() throws FunctionException, OpenMathException, CasEvaluationException,
+			CasNotAvailableException, NoRepresentationAvailableException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		OMOBJ omobj = ExpressionParser.parse("floor(12.2)", null, null);
+		OMOBJ result = OMExecutor.execute(omobj);
+		assertEquals(OMCreator.createOMI(12), result.getOMI());
+	}
+
+	@Test
+	public void testFloorIntegration2() throws FunctionException, OpenMathException, CasEvaluationException,
+			CasNotAvailableException, NoRepresentationAvailableException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		OMOBJ omobj = ExpressionParser.parse("floor(10)", null, null);
+		OMOBJ result = OMExecutor.execute(omobj);
+		assertEquals(OMCreator.createOMI(10), result.getOMI());
+	}
+
+	@Test(expected = FunctionInvalidNumberOfArgumentsException.class)
+	public void testFloorWithLessThanMinParam() throws FunctionException, OpenMathException, CasEvaluationException,
+			CasNotAvailableException, NoRepresentationAvailableException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		OMOBJ omobj = ExpressionParser.parse("floor()", null, null);
+		OMExecutor.execute(omobj);
+		fail();
+	}
+
+	@Test(expected = FunctionInvalidNumberOfArgumentsException.class)
+	public void testFloorWithMoreThanMaxParam() throws FunctionException, OpenMathException, CasEvaluationException,
+			CasNotAvailableException, NoRepresentationAvailableException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		OMOBJ omobj = ExpressionParser.parse("floor(1,3)", null, null);
+		OMExecutor.execute(omobj);
+		fail();
+	}
+	
+	@Test(expected = FunctionInvalidArgumentTypeException.class)
+	public void testFloorWithWrongArguments() throws FunctionException, OpenMathException, CasEvaluationException,
+			CasNotAvailableException, NoRepresentationAvailableException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		OMOBJ omobj = ExpressionParser.parse("floor('test')", null, null);
+		OMExecutor.execute(omobj);
+		fail();
+	}
+}
