32fc2a063e44c13d7a4224269381b66f6b2d821a
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-07-25 14:39:27 +0200

    Comments

--- src/main/java/de/uni_due/s3/evaluator/core/function/OMUtils.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/OMUtils.java
@@ -1,9 +1,11 @@
 package de.uni_due.s3.evaluator.core.function;
 
+import de.uni_due.s3.evaluator.core.functionData.OMSymbol;
 import de.uni_due.s3.evaluator.exceptions.openmath.InputMismatchException;
 import de.uni_due.s3.openmath.jaxb.OMF;
 import de.uni_due.s3.openmath.jaxb.OMI;
 import de.uni_due.s3.openmath.jaxb.OMOBJ;
+import de.uni_due.s3.openmath.jaxb.OMS;
 import de.uni_due.s3.openmath.jaxb.OMSTR;
 import de.uni_due.s3.openmath.omutils.OMConverter;
 import de.uni_due.s3.openmath.omutils.OpenMathException;
@@ -19,10 +21,10 @@ public class OMUtils {
 	 *            OMF oder OMI
 	 * @return Double Wert von OMF bzw. OMI
 	 * @throws InputMismatchException
-	 *             wenn obj kein OMF oder OMI ist
-	 * @throws OpenMathException 
+	 *             wenn obj kein OMF, OMI oder NUMS1 Symbol ist
 	 */
 	public static Double convertOMIOMFToDouble(Object obj) throws InputMismatchException {
+		// first if obj is OMOBJ get Element
 		if (obj instanceof OMOBJ) {
 			try {
 				obj = OMConverter.toElement((OMOBJ) obj);
@@ -30,11 +32,22 @@ public class OMUtils {
 				throw new InputMismatchException();
 			}
 		}
-		
+
 		if (obj instanceof OMI) {
 			return Double.parseDouble(((OMI) obj).getValue());
 		} else if (obj instanceof OMF) {
 			return ((OMF) obj).getDec();
+		} else if (obj instanceof OMS) {
+			OMS symbol = (OMS) obj;
+			if (symbol.equals(OMSymbol.NUMS1_E)) {
+				return Math.E;
+			} else if (symbol.equals(OMSymbol.NUMS1_PI)) {
+				return Math.PI;
+			} else if (symbol.equals(OMSymbol.NUMS1_INFINITY)) {
+				return Double.POSITIVE_INFINITY;
+			} else {
+				throw new InputMismatchException();
+			}
 		} else {
 			throw new InputMismatchException();
 		}
@@ -60,6 +73,14 @@ public class OMUtils {
 		}
 	}
 
+	/**
+	 * Diese Funktion nimmt einen OMI entgegen und erzeugt hieraus einen Integer
+	 * 
+	 * @param obj
+	 *            OMI
+	 * @return int
+	 * @throws InputMismatchException
+	 */
 	public static int convertOMIToInteger(Object obj) throws InputMismatchException {
 		if (obj instanceof OMOBJ) {
 			try {
@@ -68,14 +89,22 @@ public class OMUtils {
 				throw new InputMismatchException();
 			}
 		}
-		
+
 		if (obj instanceof OMI) {
 			return Integer.parseInt(((OMI) obj).getValue());
 		} else {
 			throw new InputMismatchException();
 		}
 	}
-	
+
+	/**
+	 * Diese Funktion nimmt einen OMSTR entgegen und erzeugt hieraus ein String
+	 * 
+	 * @param obj
+	 *            OMSTR
+	 * @return String rep.
+	 * @throws InputMismatchException
+	 */
 	public static String convertOMSTRToString(Object obj) throws InputMismatchException {
 		if (obj instanceof OMOBJ) {
 			try {
@@ -84,14 +113,23 @@ public class OMUtils {
 				throw new InputMismatchException();
 			}
 		}
-		
+
 		if (obj instanceof OMSTR) {
 			return ((OMSTR) obj).getContent();
 		} else {
 			throw new InputMismatchException();
 		}
 	}
-	
+
+	/**
+	 * Diese Funktion nimmt einen OMSTR, OMI, OMF entgegen und erzeugt hieraus
+	 * ein String
+	 * 
+	 * @param obj
+	 *            OMSTR, OMI oder OMF
+	 * @return String rep.
+	 * @throws InputMismatchException
+	 */
 	public static String convertOMToString(Object obj) throws InputMismatchException {
 		if (obj instanceof OMOBJ) {
 			try {
@@ -100,15 +138,14 @@ public class OMUtils {
 				throw new InputMismatchException();
 			}
 		}
-		
+
 		if (obj instanceof OMSTR) {
 			return ((OMSTR) obj).getContent();
-		}else if (obj instanceof OMI){
-			return ( (OMI)obj).getValue();
-		}else if (obj instanceof OMF){
-			return Double.toString(((OMF)obj).getDec());
-		}
-		else {
+		} else if (obj instanceof OMI) {
+			return ((OMI) obj).getValue();
+		} else if (obj instanceof OMF) {
+			return Double.toString(((OMF) obj).getDec());
+		} else {
 			throw new InputMismatchException();
 		}
 	}
