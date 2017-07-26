bef80ee2e3c7909d0d3c4386bd3020c7694e687b
Dominik Lux <dominik.lux@paluno.uni-due.de>
2017-07-26 18:45:54 +0200

    Added getDenominator and getNumerator wih tests
    Also updated Test for RandomMatrixRank

--- src/main/java/de/uni_due/s3/evaluator/core/function/openmath_jack/GetDenominator.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/openmath_jack/GetDenominator.java
@@ -2,26 +2,41 @@ package de.uni_due.s3.evaluator.core.function.openmath_jack;
 
 import java.util.List;
 
+import de.uni_due.s3.evaluator.core.dictionaries.OMSymbol;
 import de.uni_due.s3.evaluator.core.function.Function;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.openmath.jaxb.OMA;
+import de.uni_due.s3.openmath.omutils.OMTypeChecker;
 
+/**
+ * Returns the Denominator of the Division (a/b --> returns b)
+ * 
+ * @author dlux
+ *
+ */
 public class GetDenominator extends Function {
 
 	@Override
-	protected Object execute(List<Object> arguments) {
-		// TODO Auto-generated method stub
-		return null;
+	protected Object execute(List<Object> arguments) throws FunctionInvalidArgumentTypeException {
+		if (!OMTypeChecker.isOMAWithSymbol(arguments.get(0), OMSymbol.ARITH1_DIVIDE)) {
+			throw new FunctionInvalidArgumentTypeException(this, "(0)Division");
+		}
+		return ((OMA) arguments.get(0)).getOmel().get(2);
 	}
 
 	@Override
 	protected int minArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 	@Override
 	protected int maxArgs() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
+	}
+
+	@Override
+	public boolean argumentsShouldBeEvaluated() {
+		return false;
 	}
 
 }
