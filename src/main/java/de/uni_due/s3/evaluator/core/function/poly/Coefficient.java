79dd38a6db8057eb38930b0cb46c51ef9162fd6d
Dominik Lux <dominik.lux@paluno.uni-due.de>
2017-08-09 13:58:00 +0200

    fixed again some integration tests

--- src/main/java/de/uni_due/s3/evaluator/core/function/poly/Coefficient.java
+++ src/main/java/de/uni_due/s3/evaluator/core/function/poly/Coefficient.java
@@ -2,11 +2,14 @@ package de.uni_due.s3.evaluator.core.function.poly;
 
 import java.util.List;
 
+import de.uni_due.s3.evaluator.core.OMUtils;
 import de.uni_due.s3.evaluator.core.PolyUtils;
 import de.uni_due.s3.evaluator.core.function.Function;
 import de.uni_due.s3.evaluator.exceptions.cas.CasEvaluationException;
 import de.uni_due.s3.evaluator.exceptions.cas.CasNotAvailableException;
 import de.uni_due.s3.evaluator.exceptions.function.FunctionException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.evaluator.exceptions.openmath.InputMismatchException;
 import de.uni_due.s3.evaluator.exceptions.representation.NoRepresentationAvailableException;
 import de.uni_due.s3.openmath.omutils.OpenMathException;
 import de.uni_due.s3.sage.Sage;
@@ -23,6 +26,15 @@ public class Coefficient extends Function {
 	@Override
 	protected Object execute(List<Object> arguments) throws FunctionException, CasEvaluationException,
 			CasNotAvailableException, NoRepresentationAvailableException, OpenMathException {
+		try {
+			if(OMUtils.convertOMToString(arguments.get(1)).equals("") || OMUtils.convertOMToString(arguments.get(2)).equals("")) {
+				throw new FunctionInvalidArgumentTypeException("(0)Expression, (1)Variable, (2)Integer");
+			}
+			
+
+		} catch (InputMismatchException e) {
+			throw new FunctionInvalidArgumentTypeException("(0)Expression, (1)Variable, (2)Integer");
+		}
 		return Sage.evaluateInCAS(getPartialSageSyntax(arguments));
 	}
 
