a41fd18a4cfe623ff0364743a58830dbf05a2e4d
matthias.buttgereit <matthias.buttgereit@SHK-DESKTOP-23.wiwinf.uni-due.de>
2017-08-09 11:34:54 +0200

    Added last integration tests
    for packages random_jack and relation1

---
+++ src/test/java/de/uni_due/s3/evaluator/core/integration/relation1/TestLessThanOrEqual.java
@@ -0,0 +1,207 @@
+package de.uni_due.s3.evaluator.core.integration.relation1;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_due.s3.evaluator.Evaluator;
+import de.uni_due.s3.evaluator.core.integration.TestIntegration;
+import de.uni_due.s3.evaluator.exceptions.EvaluatorException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionInvalidArgumentTypeException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionNotImplementedException;
+import de.uni_due.s3.evaluator.exceptions.parser.ParserException;
+import de.uni_due.s3.evaluator.parser.ExpressionParser;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedExerciseVariableException;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedFillInVariableException;
+import de.uni_due.s3.openmath.jaxb.OMOBJ;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+
+public class TestLessThanOrEqual extends TestIntegration {
+
+	HashMap<Integer, OMOBJ> lessThanOrEqualFillInVariableMap = new HashMap<>();
+	HashMap<String, OMOBJ> lessThanOrEqualExerciseVariableMap = new HashMap<>();
+
+	@Before
+	public void beforeTest() throws FunctionNotImplementedException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		lessThanOrEqualFillInVariableMap.put(1, ExpressionParser.parse("3", null, null));
+		lessThanOrEqualFillInVariableMap.put(2, ExpressionParser.parse("1", null, null));
+		lessThanOrEqualFillInVariableMap.put(3, ExpressionParser.parse("2.5", null, null));
+
+		lessThanOrEqualExerciseVariableMap.put("a", ExpressionParser.parse("8", null, null));
+		lessThanOrEqualExerciseVariableMap.put("b", ExpressionParser.parse("25", null, null));
+		lessThanOrEqualExerciseVariableMap.put("c", ExpressionParser.parse("2.5", null, null));
+	}
+
+	@Test
+	public void testLessThanOrEqual1() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("22<=8", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("8<=8", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("2<=8", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual4() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("8<=9", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual5() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("45<=7", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual6() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("7<=7", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual7() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("-0.2<=0", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqual8() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("0<=0", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables1() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("22<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("3<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("2<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables4() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("8<=[var=b]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+		
+	@Test
+	public void testLessThanOrEqualWithVariables5() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("8<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables6() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("[var=b]<=23", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables7() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[var=a]<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables8() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[var=a]<=[var=b]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithVariables9() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("[var=b]<=[var=a]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput1() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("22<=[pos=1]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("3<=[pos=1]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("2<=[pos=1]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput4() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("3<=[pos=1]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput5() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[pos=2]<=5", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput6() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("5<=[pos=2]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testLessThanOrEqualWithInput7() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("[pos=1]<=[pos=2]", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+	}
+	
+	@Test(expected=FunctionInvalidArgumentTypeException.class)
+	public void testLessThanOrEqualWithEncapsulation1() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 <= (1 <= 2)", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+		fail();
+	}
+	
+	@Test(expected=FunctionInvalidArgumentTypeException.class)
+	public void testLessThanOrEqualWithEncapsulation2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 <= (1 <= (0 <= 1))", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+		fail();
+	}
+	
+	@Test(expected=FunctionInvalidArgumentTypeException.class)
+	public void testLessThanOrEqualWithEncapsulation3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("0 <= (0 <= (0 <= (0 <= (0 <= 1))))", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+		fail();
+	}
+	
+	@Test(expected=FunctionInvalidArgumentTypeException.class)
+	public void testLessThanOrEqualWithEncapsulation4() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("((((1 <= 1) <= 1) <= 1) <= 1) <= 1", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap));
+		fail();
+	}
+	
+	@Test(expected=ParserException.class)
+	public void testLessThanOrEqualWithWrongInputCharacter() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("6 <= ab", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap);
+		fail();
+	}
+	
+	@Test(expected=FunctionInvalidArgumentTypeException.class)
+	public void testLessThanOrEqualWithWrongInputString() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("6 <= 'a'", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap);
+		fail();
+	}
+	
+	@Test (expected=UndefinedExerciseVariableException.class)
+	public void testLessThanOrEqualWithMissingExcerciseVariable() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("[var=j] <= 2", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap);
+		fail();
+	}
+	
+	@Test (expected=UndefinedFillInVariableException.class)
+	public void testLessThanOrEqualWithMissingInput() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("[pos=42] <= 2", lessThanOrEqualExerciseVariableMap, lessThanOrEqualFillInVariableMap);
+		fail();
+	}
+}
---
+++ src/test/java/de/uni_due/s3/evaluator/core/integration/relation1/TestNotEqual.java
@@ -0,0 +1,171 @@
+package de.uni_due.s3.evaluator.core.integration.relation1;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_due.s3.evaluator.Evaluator;
+import de.uni_due.s3.evaluator.core.integration.TestIntegration;
+import de.uni_due.s3.evaluator.exceptions.EvaluatorException;
+import de.uni_due.s3.evaluator.exceptions.function.FunctionNotImplementedException;
+import de.uni_due.s3.evaluator.exceptions.parser.ParserException;
+import de.uni_due.s3.evaluator.parser.ExpressionParser;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedExerciseVariableException;
+import de.uni_due.s3.evaluator.exceptions.parser.UndefinedFillInVariableException;
+import de.uni_due.s3.openmath.jaxb.OMOBJ;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+
+public class TestNotEqual extends TestIntegration {
+
+	HashMap<Integer, OMOBJ> notEqualFillInVariableMap = new HashMap<>();
+	HashMap<String, OMOBJ> notEqualExerciseVariableMap = new HashMap<>();
+
+	@Before
+	public void beforeTest() throws FunctionNotImplementedException, UndefinedFillInVariableException,
+			UndefinedExerciseVariableException, ParserException {
+		notEqualFillInVariableMap.put(1, ExpressionParser.parse("3", null, null));
+		notEqualFillInVariableMap.put(2, ExpressionParser.parse("10", null, null));
+		notEqualFillInVariableMap.put(3, ExpressionParser.parse("3.5", null, null));
+
+		notEqualExerciseVariableMap.put("a", ExpressionParser.parse("3", null, null));
+		notEqualExerciseVariableMap.put("b", ExpressionParser.parse("10", null, null));
+		notEqualExerciseVariableMap.put("c", ExpressionParser.parse("3.5", null, null));
+	}
+
+	@Test
+	public void testNotEqual1() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 != 0", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("30 != 7", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("-10 != 3", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual4() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("1 != 1", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual5() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("0 != 0", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual6() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("'a' != 'b'", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual7() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("'' != 0", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual8() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("0 != ''", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqual9() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("0 != '0'", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithVariables1() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[var=a] != 0", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithVariables2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("30 != [var=a]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithVariables3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[var=a]!= [var=b]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithVariables4() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("[var=a] != [var=a]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithInput1() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[pos=1] != 0", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithInput2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("30 != [pos=1]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithInput3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("[pos=1] != [pos=2]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithInput4() throws EvaluatorException, OpenMathException {
+		assertTrue(!Evaluator.getBooleanResult("[pos=1] != [pos=1]", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation1() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 != (1 != 0)", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation2() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 != (1 != (1 != 0))", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation3() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 != (1 != (1 != (1 != (1 != 0))))", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation4() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("1 != (1 != (1 != (1 != (1 != 1))))", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation5() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("((((1 != 0) != 1) != 1) != 1) != 1", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test
+	public void testNotEqualWithEncapsulation6() throws EvaluatorException, OpenMathException {
+		assertTrue(Evaluator.getBooleanResult("((((1 != 1) != 1) != 1) != 1) != 1", notEqualExerciseVariableMap, notEqualFillInVariableMap));
+	}
+	
+	@Test(expected=ParserException.class)
+	public void testNotEqualWithWrongInputCharacter() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("1 == ab", notEqualExerciseVariableMap, notEqualFillInVariableMap);
+		fail();
+	}
+	
+	@Test (expected=UndefinedExerciseVariableException.class)
+	public void testNotEqualWithMissingExcerciseVariable() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("[var=j] == 2", notEqualExerciseVariableMap, notEqualFillInVariableMap);
+		fail();
+	}
+	
+	@Test (expected=UndefinedFillInVariableException.class)
+	public void testNotEqualWithMissingInput() throws EvaluatorException, OpenMathException {
+		Evaluator.getBooleanResult("[pos=42] == 2", notEqualExerciseVariableMap, notEqualFillInVariableMap);
+		fail();
+	}
+}
