640f8e374a63d5a11c3c38f469c0ac003651d242
Dominik Lux <dominik.lux@paluno.uni-due.de>
2017-08-23 09:45:20 +0200

    Small Typo Error corrected

--- src/main/java/de/uni_due/s3/evaluator2/core/dictionaries/OMSEvaluatorSyntaxDictionary.java
+++ src/main/java/de/uni_due/s3/evaluator2/core/dictionaries/OMSEvaluatorSyntaxDictionary.java
@@ -107,7 +107,7 @@ public class OMSEvaluatorSyntaxDictionary {
 		functionSymbolMap.put("expand", OMSymbol.POLYNOMIAL1_EXPAND);
 		
 		functionSymbolMap.put("random", OMSymbol.RANDOMJACK_RANDOM);
-		functionSymbolMap.put("randombetween", OMSymbol.RANDOMJACK_RANDOMBETWEEN);
+		functionSymbolMap.put("randomBetween", OMSymbol.RANDOMJACK_RANDOMBETWEEN);
 
 		functionSymbolMap.put("equal", OMSymbol.RELATION1_EQ);
 		functionSymbolMap.put("greaterthanorequal", OMSymbol.RELATION1_GEQ);
