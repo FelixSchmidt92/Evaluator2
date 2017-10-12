8066c84d480fd3864a9163c3a23d55f92cfa1a1f
Florian Richtscheid <Florian.Richtscheid@paluno.uni-due.de>
2017-10-12 11:42:20 +0200

    fixed #64 partly (just paletteRow)

--- src/test/java/de/uni_due/s3/evaluator2/core/integration/editor1/TestPaletteRow.java
+++ src/test/java/de/uni_due/s3/evaluator2/core/integration/editor1/TestPaletteRow.java
@@ -5,12 +5,16 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBException;
+
 import org.junit.Test;
 
 import de.uni_due.s3.evaluator2.Evaluator;
 import de.uni_due.s3.evaluator2.core.dictionaries.OMSymbol;
 import de.uni_due.s3.evaluator2.exceptions.EvaluatorException;
+import de.uni_due.s3.evaluator2.exceptions.function.FunctionNotImplementedException;
 import de.uni_due.s3.openmath.jaxb.OMOBJ;
+import de.uni_due.s3.openmath.omutils.OMConverter;
 import de.uni_due.s3.openmath.omutils.OMCreator;
 import de.uni_due.s3.openmath.omutils.OpenMathException;
 
@@ -22,5 +26,19 @@ public class TestPaletteRow {
 		List<Object>args = new ArrayList<Object>();
 		args.add(OMSymbol.ARITH1_PLUS);
 		assertEquals(OMCreator.createOMA(OMSymbol.EDITOR1_PALETTE_ROW, args),result.getOMA());
+	}
+	
+	@Test(expected = FunctionNotImplementedException.class) 
+	public void testPaletteRowWithNotImpelemtnedFunctions() throws JAXBException, EvaluatorException, OpenMathException {
+		OMOBJ unkownFunction = OMConverter.toObject(""
+				+ "<OMOBJ>"
+				+ "<OMA>"
+				+ "<OMS cd=\"editor1\" name=\"palette_row\"/> "
+				+ "<OMA>"
+				+ "<OMS cd=\"arith1\" name=\"unkownOperation\"/>"
+				+ "</OMA>"	
+				+ "</OMA>"
+				+ "</OMOBJ>");
+		Evaluator.evaluate(unkownFunction);
 	}
 }
