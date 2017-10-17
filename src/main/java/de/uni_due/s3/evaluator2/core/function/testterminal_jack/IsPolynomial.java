5cc8436e379ef08058df0585babcc954c5089078
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-10-17 15:03:08 +0200

    IsPolynomial repariert

--- src/main/java/de/uni_due/s3/evaluator2/core/function/testterminal_jack/IsPolynomial.java
+++ src/main/java/de/uni_due/s3/evaluator2/core/function/testterminal_jack/IsPolynomial.java
@@ -2,21 +2,17 @@ package de.uni_due.s3.evaluator2.core.function.testterminal_jack;
 
 import java.util.List;
 
-import de.uni_due.s3.evaluator2.core.dictionaries.OMSymbol;
 import de.uni_due.s3.evaluator2.core.function.Function;
+import de.uni_due.s3.evaluator2.exceptions.EvaluatorException;
 import de.uni_due.s3.evaluator2.exceptions.function.FunctionInvalidArgumentTypeException;
-import de.uni_due.s3.openmath.jaxb.OMA;
-import de.uni_due.s3.openmath.jaxb.OMF;
-import de.uni_due.s3.openmath.jaxb.OMI;
-import de.uni_due.s3.openmath.jaxb.OMS;
-import de.uni_due.s3.openmath.jaxb.OMSTR;
-import de.uni_due.s3.openmath.jaxb.OMV;
+import de.uni_due.s3.evaluator2.sage.Sage;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
 
 /**
- * @Symja This Function works (mostly) as in Symja 
+ * @Symja This Function works (mostly) as in Symja
  * 
- *             This Function checks if in given Expression the given Variable
- *             (or Number) is a Polynom
+ *        This Function checks if in given Expression the given Variable (or
+ *        Number) is a Polynom
  * 
  * @author dlux
  *
@@ -24,16 +20,9 @@ import de.uni_due.s3.openmath.jaxb.OMV;
 public class IsPolynomial extends Function {
 
 	@Override
-	protected Object execute(List<Object> arguments) throws FunctionInvalidArgumentTypeException {
-		
-		
-		if (arguments.get(0) instanceof OMSTR || arguments.get(1) instanceof OMSTR ) {
-			throw new FunctionInvalidArgumentTypeException("(0)Polynom, (1)Variable");
-		}
-		if(visitArgs(arguments.get(0), arguments.get(1))) {
-			return OMSymbol.LOGIC1_TRUE;
-		}
-		return OMSymbol.LOGIC1_FALSE;
+	protected Object execute(List<Object> arguments) throws OpenMathException, EvaluatorException {
+		Object result = Sage.evaluateInCAS(getPartialSageSyntax(arguments));
+		return result;
 	}
 
 	@Override
@@ -51,64 +40,27 @@ public class IsPolynomial extends Function {
 		return false;
 	}
 
-	private boolean visitArgs(Object obj, Object current) {
-		if (obj instanceof OMA) {
-			// Case OMA is argument
-			boolean status = true;
-			OMA oma = (OMA) obj;
-			OMS oms = (OMS) oma.getOmel().get(0);
-			for (int i = 1; i < oma.getOmel().size(); i++) {
-				if (oma.getOmel().get(i) instanceof OMA) {
-					// OMA in OMA visitArgs again
-					status = visitArgs(oma.getOmel().get(i), current);
-				} else if (oma.getOmel().get(i).equals(current)) {
-					// current OM* equals argument check if OMA'S OMS is Polynom
-					status = checkPolynomFunction(oms, i, oma.getOmel().get(i));
-				}
+	@Override
+	public String getPartialSageSyntax(List<Object> arguments) throws EvaluatorException {
+		String term = getSageSyntax(arguments.get(0));
+		String var = getSageSyntax(arguments.get(1));
 
-				// If status set to false in one of these checks above false will be returned.
-				// Somewhere current was not Polynomial
-				if (!status) {
-					return false;
-				}
-
-			}
-		} else if (!(current instanceof OMV) && !(current instanceof OMI) && !(current instanceof OMF)) {
-			// Case current is NOT OMV or OMI or OMF
-			return false;
+		if (var.length() != 1) {
+			throw new FunctionInvalidArgumentTypeException(this, "(0)Term, (1)Char");
 		}
-		
-		//last Case OMA has OM* in it (or not) which has to be Polynomial
-		//and OM* is OMV OMI or OMF
-		return true;
-	}
 
-	/**
-	 * This method decides which {@link OMSymbol} can be seen as an Polynomial
-	 * Function
-	 * 
-	 * @param oms
-	 *            the {@link OMSymbol}
-	 * @return true if Times, Divide ... false otherwise (like sqrt(x) or something
-	 *         else)s
-	 */
-	private boolean checkPolynomFunction(OMS oms, int i, Object current) {
-		if (oms.equals(OMSymbol.ARITH1_DIVIDE))
-			return true;
-		if (oms.equals(OMSymbol.ARITH1_MINUS))
-			return true;
-		if (oms.equals(OMSymbol.ARITH1_PLUS))
-			return true;
-		if (oms.equals(OMSymbol.ARITH1_POWER) && (i == 1 || !(current instanceof OMV)))
-			//iff second arg is OMV then false!
-			return true;
-		if (oms.equals(OMSymbol.ARITH1_UNARY_MINUS))
-			return true;
-		if (oms.equals(OMSymbol.ARITH1_TIMES))
-			return true;
+		String sageVar = Sage.getSagePreVariable(term + " " + var);
 
-		return false;
+		StringBuilder sb = new StringBuilder();
 
+		sb.append(sageVar);
+		sb.append("SR(");
+		sb.append(term);
+		sb.append(").is_polynomial(");
+		sb.append(var);
+		sb.append(")");
+
+		return sb.toString();
 	}
 
 }
