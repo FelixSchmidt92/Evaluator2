6d3d54de13c8581dd8cb33d2d4c679a04d7771e6
Pobel, Sebastian <sebastian.pobel@paluno.uni-due.de>
2017-10-23 19:14:03 +0200

    SageServer aktualisiert

--- src/test/java/de/uni_due/s3/evaluator2/sage/TestSage.java
+++ src/test/java/de/uni_due/s3/evaluator2/sage/TestSage.java
@@ -1,11 +1,14 @@
 package de.uni_due.s3.evaluator2.sage;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.uni_due.s3.evaluator2.core.dictionaries.OMSymbol;
 import de.uni_due.s3.evaluator2.exceptions.cas.CasEvaluationException;
 import de.uni_due.s3.evaluator2.exceptions.cas.CasException;
 import de.uni_due.s3.evaluator2.exceptions.cas.CasNotAvailableException;
@@ -27,5 +30,10 @@ public class TestSage {
 	@Test(expected = CasException.class)
 	public void testException1() throws CasEvaluationException, CasNotAvailableException, OpenMathException {
 		Sage.evaluateInCAS("");
+	}
+	
+	@Test
+	public void testExceptionEIsNotVariable() throws CasEvaluationException, CasNotAvailableException, OpenMathException {
+		assertEquals(OMSymbol.LOGIC1_TRUE, Sage.evaluateInCAS("var('e'); e.is_real()"));
 	}
 }
