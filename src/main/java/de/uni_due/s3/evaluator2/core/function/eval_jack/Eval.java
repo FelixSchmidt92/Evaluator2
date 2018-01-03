cba0585cbd2eb0e839380dbed263ad6dbd957471
Dominik Lux <dominik.lux@paluno.uni-due.de>
2018-01-03 10:30:01 +0100

    Small Bug-Fix in Eval.java

--- src/main/java/de/uni_due/s3/evaluator2/core/function/eval_jack/Eval.java
+++ src/main/java/de/uni_due/s3/evaluator2/core/function/eval_jack/Eval.java
@@ -3,6 +3,7 @@ package de.uni_due.s3.evaluator2.core.function.eval_jack;
 import java.util.List;
 
 import de.uni_due.s3.evaluator2.core.function.Function;
+import de.uni_due.s3.evaluator2.core.visitor.operation.OMToResultVisitor;
 import de.uni_due.s3.evaluator2.exceptions.EvaluatorException;
 import de.uni_due.s3.openmath.omutils.OpenMathException;
 
@@ -16,7 +17,7 @@ public class Eval extends Function {
 
 	@Override
 	protected Object execute(List<Object> arguments) throws EvaluatorException, OpenMathException {
-		return arguments.get(0);
+		return OMToResultVisitor.getInstance().visit(arguments.get(0));
 	}
 
 	@Override
