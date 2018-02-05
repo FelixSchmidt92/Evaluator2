6fb379afc7a5b05c0ab3655b4c10522a76f8380e
wakeugit <wakeuwilfried@gmail.com>
2018-02-05 10:32:01 +0100

    GenerateSentenceTransformation

---
+++ src/main/java/de/uni_due/s3/evaluator2/function/nlp/GenerateSentenceTransformation.java
@@ -0,0 +1,30 @@
+package de.uni_due.s3.evaluator2.function.nlp;
+
+import java.util.List;
+
+import de.uni_due.s3.evaluator2.exceptions.EvaluatorException;
+import de.uni_due.s3.evaluator2.function.Function;
+import de.uni_due.s3.openmath.omutils.OpenMathException;
+
+public class GenerateSentenceTransformation extends Function{
+
+	@Override
+	protected Object getPartialOpenMathElementResult(List<Object> arguments)
+			throws EvaluatorException, OpenMathException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int minArgs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected int maxArgs() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
