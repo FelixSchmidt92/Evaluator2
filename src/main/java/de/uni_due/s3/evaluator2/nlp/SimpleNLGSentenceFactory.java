c43052034ce8ad44479611bf58a821fd570b6448
FelixSchmidt92 <fesch106@gmail.com>
2018-02-05 12:41:05 +0100

    SimpleNLG

--- src/main/java/de/uni_due/s3/evaluator2/nlp/SimpleNLGSentenceFactory.java
+++ src/main/java/de/uni_due/s3/evaluator2/nlp/SimpleNLGSentenceFactory.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import de.uni_due.s3.evaluator2.nlp.exceptions.InvalidContextException;
 import de.uni_due.s3.evaluator2.nlp.exceptions.InvalidDifficultyExeption;
 import de.uni_due.s3.evaluator2.nlp.exceptions.InvalidTenseException;
+import simplenlg.lexicon.Lexicon;
+
+
 
 public class SimpleNLGSentenceFactory implements ISentenceFactory {
 	
-	
-
 	@Override
 	public ArrayList<String> createSentenceTransformation(String context, String source_tense, String target_tense,
 			  String difficulty)  throws InvalidContextException, InvalidDifficultyExeption, InvalidTenseException{
+		Lexicon lexicon = Lexicon.getDefaultLexicon();
+		
 		ArrayList<String> result = new ArrayList<String>();
 		result.add("This is the source sentence");
 		result.add("This is the target sentence");		
